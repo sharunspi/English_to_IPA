1eecae02bd2cf12c91713f6bbd8b67dd8c2eaed3
sahrun <sharunkk@eyerov.com>
2021-03-01 21:35:06 +0530

    initial commit

---
+++ english_to_ipa_translator/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import db from './db/DB'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         {
+           db[0].englishName
+         }
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
